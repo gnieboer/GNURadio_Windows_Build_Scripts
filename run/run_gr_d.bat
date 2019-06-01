94d7f5fd6b6de18e23122f38bf3b7f9f970e04f1
gnieboer <gnieboer@corpcomm.net>
2019-06-01 09:32:25 -0400

    Changes to pythonpath for 3.8

--- run/run_gr_d.bat
+++ run/run_gr_d.bat
@@ -1,10 +1,16 @@
 @echo off
 echo setting gnuradio environment
 
+REM --- Change the below to 3.7 for 3.7 releases
+set GR_VERSION=3.7
+
 REM --- Set Python environment ---
 set PYTHONHOME=%~dp0..\gr-python27
-set PYTHONPATH=%~dp0..\gr-python27\Lib\site-packages;%~dp0..\gr-python27\dlls;%~dp0..\gr-python27\libs;%~dp0..\gr-python27\lib;%~dp0..\lib\site-packages;%~dp0..\gr-python27\Lib\site-packages\pkgconfig;%~dp0..\gr-python27\Lib\site-packages\gtk-2.0\glib;%~dp0..\gr-python27\Lib\site-packages\gtk-2.0;%~dp0..\gr-python27\Lib\site-packages\wx-3.0-msw;%~dp0..\gr-python27\Lib\site-packages\sphinx;%~dp0..\gr-python27\Lib\site-packages\lxml-3.4.4-py2.7-win.amd64.egg
-
+if "%GR_VERSION%" == "3.7" (
+	set PYTHONPATH=%~dp0..\gr-python27\Lib\site-packages;%~dp0..\gr-python27\dlls;%~dp0..\gr-python27\libs;%~dp0..\gr-python27\lib;%~dp0..\lib\site-packages;%~dp0..\gr-python27\Lib\site-packages\pkgconfig;%~dp0..\gr-python27\Lib\site-packages\gtk-2.0\glib;%~dp0..\gr-python27\Lib\site-packages\gtk-2.0;%~dp0..\gr-python27\Lib\site-packages\wx-3.0-msw;%~dp0..\gr-python27\Lib\site-packages\sphinx;%~dp0..\gr-python27\Lib\site-packages\lxml-3.4.4-py2.7-win.amd64.egg	
+) else (
+	set PYTHONPATH=%~dp0..\gr-python27\Lib\site-packages;%~dp0..\gr-python27\dlls;%~dp0..\gr-python27\libs;%~dp0..\gr-python27\lib;%~dp0..\lib\site-packages;%~dp0..\lib\site-packages\gnuradio;%~dp0..\lib\site-packages\gnuradio\qtgui;%~dp0..\gr-python27\Lib\site-packages\pkgconfig;%~dp0..\gr-python27\Lib\site-packages\sphinx;%~dp0..\gr-python27\Lib\site-packages\lxml-3.4.4-py2.7-win.amd64.egg	
+)
 set PATH=%~dp0;%~dp0..\gr-python27\dlls;%~dp0..\gr-python27;%PATH%
 
 REM --- Set GRC environment ---
